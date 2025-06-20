668ecd4af7bf682421ce86c3eebc98944b9dd727
AsiyaKondusa <asiyaabdalla2004@gmail.com>
2025-06-20 20:58:22 +0300

    Update test_core_queue.py

--- .github/test_core_queue.py
+++ .github/test_core_queue.py
@@ -1,14 +1,14 @@
-from core_queue import  PrintQueueManager 
+from print_queue_manager import  PrintQueueManager 
 
-cq =  PrintQueueManager()(capacity=3)
+pq =  PrintQueueManager()(capacity=3)
 
-cq.enqueue_job("user1", "job101", 2)
-cq.enqueue_job("user2", "job102", 3)
-cq.show_status()
+pq.enqueue_job("user1", "job101", 2)
+pq.enqueue_job("user2", "job102", 3)
+pq.show_status()
 
-cq.dequeue_job()
-cq.show_status()
+pq.dequeue_job()
+pq.show_status()
 
-cq.enqueue_job("user3", "job103", 1)
-cq.enqueue_job("user4", "job104", 4)
-cq.show_status()
+pq.enqueue_job("user3", "job103", 1)
+pq.enqueue_job("user4", "job104", 4)
+pq.show_status()
