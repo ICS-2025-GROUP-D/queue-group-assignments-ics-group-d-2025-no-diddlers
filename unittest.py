129adde292ad7e5a1dcc8c1f8592ae940d276a2a
Sean-Nguyo <seanmikenguyo@gmail.com>
2025-06-24 21:41:21 +0300

    Update unittest.py to import queue manager

--- unittest.py
+++ unittest.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from queue import PrintQueueManager
+from  Priority_AgingSystem import PrintQueueManager
 from io import StringIO
 
 #Test case for PrintQueueManager to check empty queue visualization
